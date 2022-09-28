ef214cde7c37f408643a20aaaa3bb5b08eec906b
”Andrey <okolefleef@disr.it>
2022-09-28 21:57:45 +0700

    Partial refactoring.

--- src/config.py
+++ src/config.py
@@ -11,6 +11,8 @@ from pathlib import Path
 
 
 path_workdir = Path(__file__).resolve().parent.parent
+path_workdir_fj = path_workdir / "data" / "files.json"
+path_workdir_env = path_workdir / "data" / ".env"
 
 try:
     from pydantic import BaseSettings, SecretStr, ValidationError
@@ -32,17 +34,17 @@ class Settings(BaseSettings):
     SECRETKEYFILE: SecretStr
 
     class Config:
-        env_file = path_workdir / "data/.env"
+        env_file = path_workdir_env
 
 
 def get_env() -> Settings:
-    if not (path_workdir / "data" / "files.json").is_file():
-        (path_workdir / "data" / "files.json").write_text(dedent("""\
+    if not path_workdir_fj.is_file():
+        path_workdir_fj.write_text(dedent("""\
             {}
         """))
 
-    if not (path_workdir / "data" / ".env").is_file():
-        (path_workdir / "data" / ".env").write_text(dedent("""\
+    if not path_workdir_env.is_file():
+        path_workdir_env.write_text(dedent("""\
             TG_APP_ID=
             SESSION_NAME=""
             DEBUG=False
